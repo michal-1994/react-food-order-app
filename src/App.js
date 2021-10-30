43873af421b4bc777e3d2f149ff87484dd2cae98
Michal <michalgrzegorczyk04@gmail.com>
2021-10-30 15:47:36 +0200

    Add Header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import React from "react";
+
+import Header from "./components/Layout/Header"
+
 function App() {
   return (
-    <div>
+    <React.Fragment>
+      <Header />
       <h2>Let's get started!</h2>
-    </div>
+    </React.Fragment>
   );
 }
 
