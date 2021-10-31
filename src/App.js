743a11c9b50f55899d17e3883eb9ec20566d29ad
Michal <michalgrzegorczyk04@gmail.com>
2021-10-31 07:40:56 +0100

    Add Meals component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
 
-import Header from "./components/Layout/Header"
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+
 
 function App() {
-  return (
-    <React.Fragment>
-      <Header />
-      <h2>Let's get started!</h2>
-    </React.Fragment>
-  );
+	return (
+		<React.Fragment>
+			<Header />
+			<main>
+				<Meals />
+			</main>
+		</React.Fragment>
+	);
 }
 
 export default App;
